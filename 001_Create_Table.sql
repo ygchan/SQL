334536df9bc23035574ba58274778584f8b5d0a6
George Chan <ygchan89@gmail.com>
2019-08-08 21:16:25 -0400

    Delete

--- 001_Create_Table.sql
+++
@@ -1,9 +0,0 @@
--- Table with member's information
-create table member
-(
-	member_id smallint unsigned,
-    fname varchar(20),
-    lname varchar(20),
-    gender char(1),
-    constraint pk_member primary key (member_id)
-);
