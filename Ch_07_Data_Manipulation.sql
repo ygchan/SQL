f8a3f407c43d23e8d37a63f377e2a9d52ba6e8db
ygchan <ygchan89@gmail.com>
2019-08-10 10:24:50 -0400

    Learned: position('a' in 'abc), locate('a', 'abc', 2), strcmp

--- Ch_07_Data_Manipulation.sql
+++ Ch_07_Data_Manipulation.sql
@@ -42,6 +42,112 @@ mysql> show warnings;
 -- server only allocates enough space to store the string, it is not wasteful
 -- to set a high upper limit. This is different from SAS and is great!
 
--- 03: How to escape a singe quote?
+-- 03: How to escape a singe quote and retrieve it?
 update string_tbl
    set text_fld = 'This string did''t work, but it does now!';
+
+-- When pull data such as notes entered by users, you may want to use 
+-- quote function to get all the non-system-generated characters.
+select quote(text_fld)
+from string_tbl;
+
+/* Output:
++---------------------------------------------+
+| quote(text_fld)                             |
++---------------------------------------------+
+| 'This string did\'t work, but it does now!' |
++---------------------------------------------+
+1 row in set (0.02 sec)
+*/
+
+-- String Manipulation
+delete from string_tbl;
+
+insert into string_tbl (char_fld, vchar_fld, text_fld)
+   values ('This string is 28 characters',
+      'This string is 28 characters',
+      'This string is 28 characters');
+
+-- 04: Most common function - Length of string
+select length(char_fld) char_length,
+   length(vchar_fld) varchar_length,
+   length(text_fld) text_length
+from string_tbl;
+
+/* Output:
++-------------+----------------+-------------+
+| char_length | varchar_length | text_length |
++-------------+----------------+-------------+
+|          28 |             28 |          28 |
++-------------+----------------+-------------+
+1 row in set (0.01 sec)
+*/
+
+-- 05: Finding the position of the substring
+select position('characters' in vchar_fld),
+   /* SQL is 1 based, unlike C++/Python 0 based
+      If result is not found, it is 0 */
+   position('not here' in vchar_fld)
+from string_tbl;
+
+/* Output:
++-------------------------------------+-----------------------------------+
+| position('characters' in vchar_fld) | position('not here' in vchar_fld) |
++-------------------------------------+-----------------------------------+
+|                                  19 |                                 0 |
++-------------------------------------+-----------------------------------+
+1 row in set (0.00 sec)
+*/
+
+-- 06: Finding the position with optional starting position
+select locate('is', vchar_fld, 5)
+from string_tbl;
+
+-- 07: MySQL's string comparison function strcmp()
+--    returns -1 if first string comes before second string in sort order.
+--    returns 0  if first string and second string are identical
+--               strcmp() function are case insensitive.
+--    returns 1  if first string comes after second string in sort order.
+
+
+delete from string_tbl;
+insert into string_tbl(vchar_fld) values ('abcd');
+insert into string_tbl(vchar_fld) values ('xyz');
+insert into string_tbl(vchar_fld) values ('QRSTUV');
+insert into string_tbl(vchar_fld) values ('qrstuv');
+insert into string_tbl(vchar_fld) values ('12345');
+
+select vchar_fld
+from string_tbl
+order by vchar_fld;
+
+/* Output:
++-----------+
+| vchar_fld |
++-----------+
+| 12345     |
+| abcd      |
+| QRSTUV    |
+| qrstuv    |
+| xyz       |
++-----------+
+5 rows in set (0.00 sec)
+*/
+
+select strcmp('12345', '12345') 12345_12345, --  0 (identfical)
+   strcmp('abcd', 'xyz') abcd_xyz,           -- -1 (abcd comes before xyz)
+   strcmp('abcd', 'qrstuv') abcd_qrstuv,     -- -1 (abcd comes before qrstuv)
+   strcmp('qrstuv', 'QRSTUV') qrstuv_QRSTUV, --  0 (case insensitive comparision)
+   strcmp('12345', 'xyz') 12345_xyz,         -- -1 (numbers comes before characters)
+   strcmp('xyz', 'qrstuv') xyz_qrstuv;       --  1 (xyz comes after qrstuv)
+
+/* Output:
++-------------+----------+-------------+---------------+-----------+------------+
+| 12345_12345 | abcd_xyz | abcd_qrstuv | qrstuv_QRSTUV | 12345_xyz | xyz_qrstuv |
++-------------+----------+-------------+---------------+-----------+------------+
+|           0 |       -1 |          -1 |             0 |        -1 |          1 |
++-------------+----------+-------------+---------------+-----------+------------+
+1 row in set (0.00 sec)
+*/
+
+
