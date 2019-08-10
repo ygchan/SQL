be29e839d78f8f0ec168a33385c5b9e82e6bab7a
ygchan <ygchan89@gmail.com>
2019-08-10 14:15:06 -0400

    Completed Ch7

--- Ch_01_Summary.sql
+++ Ch_01_Summary.sql
@@ -2,23 +2,44 @@
 -- Learning SQL, 2nd Edition 
 
 /* 
-	http://shop.oreilly.com/product/9780596520847.do
-	Learning SQL, 2nd Edition
-	Master SQL Fundamentals
+   http://shop.oreilly.com/product/9780596520847.do
+   Learning SQL, 2nd Edition
+   Master SQL Fundamentals
 
-	By Alan Beaulieu
-	Publisher: O'Reilly Media
-	Release Date: April 2009
+   By Alan Beaulieu
+   Publisher: O'Reilly Media
+   Release Date: April 2009
 
-	Pages: 338
+   Pages: 338
 */
 
 -- Chapter 7: Data Generation, Conversion and Manipulation
--- 		length()
---		locate()
--- 		show warnings
---		concat()
--- 		position()
---		like '%substring%' in select context
---		ssn regex '.{3}-.{2}-.{4}' is_ss_no_format
---      insert()
+--    For strings:
+--    length()
+--    locate()
+--    show warnings
+--    concat()
+--    position()
+--    like '%substring%' in select context
+--    ssn regex '.{3}-.{2}-.{4}' is_ss_no_format
+--    insert()
+
+--    For numbers:
+--    ceil(), floor()
+--    round(), round(var, precision)
+--    truncate(var, precision)
+--    sign()
+--    abs()
+
+--    For temporal data:
+--    cast(),           ex: cast('2019-08-12' as date)
+--    str_to_date(),    ex: str_to_date('September 17, 2008', '%M %d, %Y')
+--    current_date(),
+--    current_time(), 
+--    current_timestamp()
+--    date_add()
+--    last_day()
+--    dayname()
+--    extract(),       ex: extract(year from '2018-05-01')
+--    datediff(),      ex: datediff(current_date(), '1999-01-01')
+--    cast(),          ex: cast('1234' as unsigned integer)
