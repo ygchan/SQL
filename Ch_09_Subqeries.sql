0374d6002682befb252a68811170819374ae8cd3
ygchan <ygchan89@gmail.com>
2019-08-10 21:46:03 -0400

    Add a file

---
+++ Ch_09_Subqeries.sql
@@ -0,0 +1,5 @@
+-- Subqueries
+-- A subquery is a query contained within another SQL statement
+-- It can happens in where/from claues. It is also always enclosed within parentheses.
+
+-- Usually executed prior to the containing statement.
