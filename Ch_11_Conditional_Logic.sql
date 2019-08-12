cd6a97ff4b8e442addcb5c5e8294898b0da8e3c9
ygchan <ygchan89@gmail.com>
2019-08-12 19:22:01 -0400

    Learned: Condition update, check signs and current_timestamp()

--- Ch_11_Conditional_Logic.sql
+++ Ch_11_Conditional_Logic.sql
@@ -384,7 +384,139 @@ select c.cust_id, c.fed_id, c.cust_type_cd,
   end has_saving
 from customer c;
 
+-- The next example consider how many rows are encountered.
+-- 05. Use a simple case expression to count the number of each customers
+-- and returns either 'None', '1', '2', or 3+
+select c.cust_id, c.fed_id, c.cust_id,
+  case (select count(*) from account a
+    where a.cust_id = c.cust_id)
+    when 0 then 'Non'
+    when 1 then '1'
+    when 2 then '2'
+    else '3+'
+  end num_accounts
+from customer c;
 
+/* Output:
++---------+-------------+---------+--------------+
+| cust_id | fed_id      | cust_id | num_accounts |
++---------+-------------+---------+--------------+
+|       1 | 111-11-1111 |       1 | 3+           |
+|       2 | 222-22-2222 |       2 | 2            |
+|       3 | 333-33-3333 |       3 | 2            |
+|       4 | 444-44-4444 |       4 | 3+           |
+|       5 | 555-55-5555 |       5 | 1            |
+|       6 | 666-66-6666 |       6 | 2            |
+|       7 | 777-77-7777 |       7 | 1            |
+|       8 | 888-88-8888 |       8 | 2            |
+|       9 | 999-99-9999 |       9 | 3+           |
+|      10 | 04-1111111  |      10 | 2            |
+|      11 | 04-2222222  |      11 | 1            |
+|      12 | 04-3333333  |      12 | 1            |
+|      13 | 04-4444444  |      13 | 1            |
++---------+-------------+---------+--------------+
+13 rows in set (0.01 sec)
+*/
+
+-- 06. You should always wrap your denominators in a conditional logics
+-- This is to safe guard division by zero errors.
+
+-- First let's build the subquery that calculate the avaiable total balance
+select a.product_cd, sum(a.avail_balance) tot_balance
+from account a
+group by a.product_cd;
+
+/* Output:
++------------+-------------+
+| product_cd | tot_balance |
++------------+-------------+
+| BUS        |     9345.55 |
+| CD         |    19500.00 |
+| CHK        |    73008.01 |
+| MM         |    17045.14 |
+| SAV        |     1855.76 |
+| SBL        |    50000.00 |
++------------+-------------+
+6 rows in set (0.01 sec)
+*/
+
+select a.cust_id, a.product_cd, 
+  a.avail_balance / (
+    case
+      when prod_tots.tot_balance = 0 then 1
+      else prod_tots.tot_balance
+    end
+  ) percent_of_total
+from account a inner join
+  (
+    select a.product_cd, sum(a.avail_balance) tot_balance
+    from account a
+    group by a.product_cd
+  ) prod_tots on (a.product_cd = prod_tots.product_cd);
+
+/* Output:
++---------+------------+------------------+
+| cust_id | product_cd | percent_of_total |
++---------+------------+------------------+
+|      10 | BUS        |         0.000000 |
+|      11 | BUS        |         1.000000 |
+|       1 | CD         |         0.153846 |
+|       6 | CD         |         0.512821 |
+|       7 | CD         |         0.256410 |
+|       9 | CD         |         0.076923 |
+|       1 | CHK        |         0.014488 |
+|       2 | CHK        |         0.030928 |
+|       3 | CHK        |         0.014488 |
+|       4 | CHK        |         0.007316 |
+|       5 | CHK        |         0.030654 |
+|       6 | CHK        |         0.001676 |
+|       8 | CHK        |         0.047764 |
+|       9 | CHK        |         0.001721 |
+|      10 | CHK        |         0.322911 |
+|      12 | CHK        |         0.528052 |
+|       3 | MM         |         0.129802 |
+|       4 | MM         |         0.321915 |
+|       9 | MM         |         0.548282 |
+|       1 | SAV        |         0.269431 |
+|       2 | SAV        |         0.107773 |
+|       4 | SAV        |         0.413723 |
+|       8 | SAV        |         0.209073 |
+|      13 | SBL        |         1.000000 |
++---------+------------+------------------+
+24 rows in set (0.01 sec)
+*/
+
+update account
+  set last_activity_date = current_timestamp(),
+    -- If it is debit, flip the sign
+    -- then multiple the transcation amount by sign (+1/-1) and add to
+    -- the current pending amount.
+    pending_balance = pending_balance + (
+      select t.amount *
+        case t.txn_type_cd when 'DBT' then -1 else 1 end
+      from transcation t
+      where t.txn_id = 999
+    ), 
+    -- If the fund is available in a future time, then it is count as 0
+    -- else multiply the transcation amount by the sign (+1/-1) and add to
+    -- the current available amount.
+    avail_balance = avail_balance + (
+      select
+        case t.funds_avail_date > current_timestamp() then 0
+        else (
+          t.amount *
+          case t.txn_type_cd when 'DBT' then -1 else 1 end
+        )
+        end
+      from transcation t
+      where t.txn_id = 999
+    )
+  -- Only update records that is associated with this id
+  where account.account_id = (
+    select t.account_id
+    from transaction t
+    where t.txn_id = 999
+  );
 
 
 
