7ee54b76d2b4e9961a3256775a4d3b855bbdabd3
ygchan <ygchan89@gmail.com>
2019-08-25 16:14:01 -0400

    Learned: schema dot: tables, views, columns, statistics

--- Ch_15_Metadata.sql
+++ Ch_15_Metadata.sql
@@ -86,3 +86,93 @@ order by 1;
 +------------------------+--------------+
 8 rows in set (0.02 sec)
 */
+
+-- If I want to exclude any views, simply add another condition to the where 
+-- clause. 
+
+select table_name, table_type
+from information_schema.tables
+where table_schema = 'bank' and table_type = 'BASE TABLE'
+order by 1;
+
+/* Output:
++------------------+------------+
+| table_name       | table_type |
++------------------+------------+
+| account          | BASE TABLE |
+| branch           | BASE TABLE |
+| business         | BASE TABLE |
+| claims           | BASE TABLE |
+| customer         | BASE TABLE |
+| customer_totals  | BASE TABLE |
+| department       | BASE TABLE |
+| employee         | BASE TABLE |
+| individual       | BASE TABLE |
+| number_tbl       | BASE TABLE |
+| officer          | BASE TABLE |
+| product          | BASE TABLE |
+| product_type     | BASE TABLE |
+| some_transcation | BASE TABLE |
+| string_tbl       | BASE TABLE |
+| t                | BASE TABLE |
+| transaction      | BASE TABLE |
++------------------+------------+
+17 rows in set (0.01 sec)
+*/
+
+-- information about columns (in columns view)
+
+select column_name, data_type, character_maximum_length char_max_len,
+   numeric_precision num_prcsn, numeric_scale num_scale
+from information_schema.columns
+where table_schema = 'bank' and table_name = 'account'
+order by ordinal_position;
+
+-- information about indexes (in statistics view)
+
+select index_name, non_unique, seq_in_index, column_name
+from information_schema.statistics
+where table_schema = 'bank' and table_name = 'account'
+order by 1, 3;
+
+/* Output:
++----------------+------------+--------------+----------------+
+| index_name     | non_unique | seq_in_index | column_name    |
++----------------+------------+--------------+----------------+
+| acc_bal_idx    |          1 |            1 | cust_id        |
+| acc_bal_idx    |          1 |            2 | avail_balance  |
+| cust_prod_idx  |          0 |            1 | cust_id        |
+| cust_prod_idx  |          0 |            2 | product_cd     |
+| fk_a_branch_id |          1 |            1 | open_branch_id |
+| fk_a_emp_id    |          1 |            1 | open_emp_id    |
+| fk_product_cd  |          1 |            1 | product_cd     |
+| PRIMARY        |          0 |            1 | account_id     |
++----------------+------------+--------------+----------------+
+8 rows in set (0.00 sec)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
